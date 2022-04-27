fefa3bfa50a6f0211c093e7ca32d242043856bd0
Isaac Khan <isaackhan21@gmail.com>
2022-04-27 11:15:59 +0100

    Added Layout Component

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto my-7">{children}</main>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Welcome to Isaac Khan's Blog",
+  keywords:
+    "Isaac Khan, Isaac, Khan, development, coding, Web Development, web",
+  description: "The Best Info and News In Web Dev",
+};
